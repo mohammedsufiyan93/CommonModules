321f687396b01e65702747190001afe65371ed0a
Mohammed Sufiyan <mohammedsufiyan08@outlook.com>
2022-06-02 17:39:34 +0530

    Login

---
+++ Login Management MVC/Login Management/Controllers/Signin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_Management.Controllers
+{
+    public class Signin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
